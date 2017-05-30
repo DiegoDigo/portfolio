cce76a2c9c6ad22a324ff944ccde1aed7fe5ab07
diego.delmiro <di3g0d0ming05@gmail.com>
2017-05-30 15:27:53 -0300

    ajustando leyout

--- home/views.py
+++ home/views.py
@@ -8,6 +8,7 @@ from django.http import Http404
 def home(request):
     contexto = {
         'dado':  get_object_or_404(models.DadosPessoal, pk=1),
+        'nivel': range(1, 6),
     }
     return render(request, 'apresentacao.html', contexto)
 
